c5c07d93b72931ac2f9ea1ed38e2773ead7bc6a7
hye-ri <kwonhr01@naver.com>
2022-08-28 23:20:04 +0900

    useFullscreen 수정

--- src/useFullscreen.js
+++ src/useFullscreen.js
@@ -1,36 +1,60 @@
 import React, { useRef } from "react";
 
-const useFullscreen = (callback) => {
+const useFullscreen = (onFulls) => {
     const element = useRef();
+
+    const doCallback = (isFullscreen) => {
+        if (onFulls && typeof onFulls === "function") {
+            onFulls(isFullscreen);
+        }
+    };
+
     const triggerFull = () => {
-        if (element.current) {
-            element.current.requestFullscreen();
-            if (callback && typeof callback === "function") {
-                callback(true);
+        const currentElem = element.current;
+        if (currentElem) {
+            if (currentElem.requestFullScreen) {
+                currentElem.requestFullscreen();
+            } else if (currentElem.mozRequestFullScreen) {
+                currentElem.mozRequestFullScreen();
+            } else if (currentElem.webkitRequestFullscreen) {
+                currentElem.webkitRequestFullscreen();
+            } else if (currentElem.msRequestFullscreen) {
+                currentElem.msRequestFullscreen();
             }
+
+            doCallback(true);
         }
     };
-    const exitFull = () => {
-        document.exitFullscreen();
-        if (callback && typeof callback === "function") {
-            callback(false);
+
+    const triggerExit = () => {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
         }
+
+        doCallback(false);
     };
-    return { element, triggerFull, exitFull };
+
+    return { element, triggerFull, triggerExit };
 };
 
 function App() {
-    const callback = (isFull) => {
+    const onFulls = (isFull) => {
         /* isFull은 boolean값 */
         console.log(isFull ? "we are full" : "we are small");
     };
-    const { element, triggerFull, exitFull } = useFullscreen(callback);
+    const { element, triggerFull, triggerExit } = useFullscreen(onFulls);
     return (
         <div>
             <div ref={element}>
                 <h1>hi</h1>
             </div>
-            <button onClick={exitFull}>Exit Fullscreen</button>
+            <button onClick={triggerExit}>Exit Fullscreen</button>
             <button onClick={triggerFull}>Make Fullscreen</button>
         </div>
     );
