84dd574589c906e254f1a4500691631009c8169b
hye-ri <kwonhr01@naver.com>
2022-08-28 20:51:17 +0900

    useBeforeLeave 추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const useBeforeLeave = () => {
-    const listener = (e) => {
-        e.preventDefault();
-        e.returnValue = "";
+const useBeforeLeave = (onLeave) => {
+    useEffect(() => {
+        document.addEventListener("mouseleave", handle);
+        return () => document.removeEventListener("mouseleave", handle);
+    }, []);
+    const handle = (e) => {
+        //console.log(e);
+        const { clientY } = e;
+        if (clientY <= 0) {
+            onLeave();
+        }
     };
-    const enablePrevent = () => window.addEventListener("beforeunload", listener);
-    const disablePrevent = () => window.removeEventListener("beforeunload", listener);
-    return { enablePrevent, disablePrevent };
+    if (typeof onLeave !== "function") {
+        return;
+    }
 };
 
 function App() {
-    const { enablePrevent, disablePrevent } = useBeforeLeave();
+    const begForLife = () => console.log("Pls don't leave");
+    useBeforeLeave(begForLife);
     return (
         <div>
-            <button onClick={enablePrevent}>protect</button>
-            <button onClick={disablePrevent}>unprotect</button>
+            <h1>Hello</h1>
         </div>
     );
 }
