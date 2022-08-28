24c43294b61a13193f911cac6e9622ecf54657a0
hye-ri <kwonhr01@naver.com>
2022-08-28 21:25:58 +0900

    useFadeIn

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
-const useBeforeLeave = (onLeave) => {
+const useFadeIn = (duration = 1, delay = 0) => {
+    const element = useRef();
     useEffect(() => {
-        document.addEventListener("mouseleave", handle);
-        return () => document.removeEventListener("mouseleave", handle);
-    }, []);
-    const handle = (e) => {
-        //console.log(e);
-        const { clientY } = e;
-        if (clientY <= 0) {
-            onLeave();
+        if (element.current) {
+            const { current } = element;
+            current.style.transition = `all ${duration}s ease-in-out ${delay}s`;
+            current.style.opacity = 1;
         }
-    };
-    if (typeof onLeave !== "function") {
+    }, []);
+    if (typeof duration !== "number" || typeof delay !== "number") {
         return;
     }
+    return { ref: element, style: { opacity: 0 } };
 };
 
 function App() {
-    const begForLife = () => console.log("Pls don't leave");
-    useBeforeLeave(begForLife);
+    const fadeInh1 = useFadeIn(0, 1);
+    const fadeInp = useFadeIn(5, 5);
     return (
         <div>
-            <h1>Hello</h1>
+            <h1 {...fadeInh1}>Hello</h1>
+            <p {...fadeInp}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, deserunt unde! Hic vitae, ipsam eum
+                commodi dicta sint, nulla ratione voluptatibus saepe, repellendus accusantium illum voluptas at quia
+                quos nam?
+            </p>
         </div>
     );
 }
